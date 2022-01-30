b94bea778ace33900fd9be6a093bdd8f928904ce
Erick Santos <erickosantos.dev@gmail.com>
2022-01-30 20:22:35 -0300

    config project

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const rescue = require('express-rescue');
+
 require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
