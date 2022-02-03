0317b473a2fb021bab70697c0bda3587fc77d573
Erick Santos <erickosantos.dev@gmail.com>
2022-02-03 19:26:46 -0300

    8 - Escreva testes para cobrir 35% das camadas da sua aplicação

---
+++ test/unit/services.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+const sinon = require('sinon');
+const { expect } = require('chai');
+const connection = require('../../models/connection');
+const {
+  getAllProducts,
+  createProduct,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createSale,
+} = require('../../services');
+
+
+describe('Testing services', () => {
+  describe('Product services', () => {
+    beforeEach(async () => {
+      const execute = [[]];
+  
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+  
+    afterEach(async () => connection.execute.restore());
+    
+    describe('Testing add product', () => {
+      it('Is an object', async () => {
+        const response = await createProduct('Monitor', 2);
+  
+        expect(response).to.be.an('object');
+      });
+    });
+  
+    describe('Testing getting all products', () => {
+      it('Array returned', async () => {
+        const response = await getAllProducts();
+  
+        expect(response).to.be.an('array');
+      });
+    });
+  
+    describe('Testing getting product by id', () => {
+      it('Is an object', async () => {
+        const prod = await createProduct('Cellphone', 2);
+        const response = await getProductById(prod.id);
+  
+        expect(response).to.be.an('object');
+      });
+    });
+  
+    describe('Testing deleting product', () => {
+      it('Deleted object', async () => {
+        const prod = await createProduct('Computer', 2);
+  
+        await deleteProduct(prod.id);
+        const result = await getAllProducts();
+        expect(result).to.not.include(prod);
+      });
+    });
+  
+    describe('Testing updating product', () => {
+      it('Product updated', async () => {
+        const prod = await createProduct('Echo dot', 2);
+        await updateProduct(prod.id, 'Alexa Echo Dot', 3);
+        
+        const updated = await getProductById(prod.id);
+        expect(updated).to.deep.include({id: prod.id, name: 'Alexa Echo Dot', quantity: 3});
+      })
+    });
+  });
+
+  describe('Sale services', () => {
+    beforeEach(async () => {
+      const execute = [[]];
+  
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+  
+    afterEach(async () => connection.execute.restore());
+
+    describe('Testing add sale', async () => {
+      const prod = await createProduct('Pendrive 8gb', 2);
+      const sale = await createSale([{ product_id: prod.id, quantity: 1 }]);
+
+      expect(sale).to.be.a('number');
+    });
+  })
+
+});
