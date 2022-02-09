b2057f40c81b52dcb8e67ec55420673aa91e10a0
Erick Santos <erickosantos.dev@gmail.com>
2022-02-09 16:43:11 -0300

    11 - Atualize a quantidade de produtos

--- services/Sale.js
+++ services/Sale.js
@@ -4,18 +4,32 @@ const { saleModel: model } = require('../models');
 // Importação de productService
 const productService = require('./Product');
 
+// Utils
+const updateQuant = async (sale, op) => {
+  const promisesUpdate = sale
+    .map(async (p) => {
+      const product = await productService.getById(p.product_id);
+      if (op === '-') product.quantity -= p.quantity;
+      else product.quantity += p.quantity;
+      await productService.updateQuant(p.product_id, product.quantity);
+    });
+  await Promise.all(promisesUpdate);
+};
+
 // Services
 const create = async (sale) => {
-  const promises = sale.map(async (p) => {
+  const promisesValidate = sale.map(async (p) => {
     const quant = await productService.getQuant(p.product_id);
     if (quant < p.quantity) {
       return true;
     }
   });
-  const products = await Promise.all(promises);
+  const products = await Promise.all(promisesValidate);
   const quantValidation = products.some((val) => val);
 
   if (quantValidation) return false;
+
+  await updateQuant(sale, '-');
   return model.create(sale);
 };
 const getAll = async () => model.getAll();
@@ -24,6 +38,8 @@ const update = async (id, quantity, saleId) => model.update(id, quantity, saleId
 const remove = async (id) => {
   const result = await model.getById(id);
   if (result.length === 0) return null;
+
+  await updateQuant(result, '+');
   await model.remove(id);
   return result;
 };
