a5f751522ba53c9e44c59593aeb82f6a027dd089
Erick Santos <erickosantos.dev@gmail.com>
2022-02-15 01:30:28 -0300

    13 - Escreva testes para cobrir 50% das camadas da sua aplicação

--- test/unit/services.js
+++ test/unit/services.js
@@ -200,6 +200,171 @@ describe('Services test', () => {
   });
 
   describe('Test Sale Service', () => {
+    describe('Creating a product', () => {
+      describe('If quantity on stock less than sold', () => {
+        const payloadCreate = [{
+          product_id: 1,
+          quantity: 5,
+        }];
+        before(() => {
+          sinon.stub(productService, 'getQuant').resolves(2);
+        });
+  
+        after(() => {
+          productService.getQuant.restore();
+        });
 
+        it('Returns false', async () => {
+          const result = await saleService.create(payloadCreate);
+          expect(result).to.be.false;
+        });
+      });
+      describe('Sale created', () => {
+        const payloadCreate = [{
+          product_id: 1,
+          quantity: 2,
+        }];
+        before(() => {
+          sinon.stub(productService, 'getQuant').resolves(3);
+          sinon.stub(saleModel, 'create').resolves(1);
+          sinon.stub(productService, 'getById').resolves({ quantity: 2 });
+          sinon.stub(productService, 'updateQuant').resolves();
+        });
+  
+        after(() => {
+          saleModel.create.restore();
+          productService.getById.restore();
+          productService.getQuant.restore();
+          productService.updateQuant.restore();
+        });
+
+        it('Returns saleId', async () => {
+          const result = await saleService.create(payloadCreate);
+          expect(result).to.be.equal(1);
+        });
+      });
+    });
+
+    describe('Getting all sales', () => {
+      const payload = [
+        {
+          "saleId": 1,
+          "date": "2021-09-09T04:54:29.000Z",
+          "product_id": 1,
+          "quantity": 2
+        },
+        {
+          "saleId": 1,
+          "date": "2021-09-09T04:54:54.000Z",
+          "product_id": 2,
+          "quantity": 2
+        }
+      ];
+      before(() => {
+        sinon.stub(saleModel, 'getAll').resolves(payload);
+      });
+
+      after(() => {
+        saleModel.getAll.restore();
+      });
+
+      it('Returns all sales', async () => {
+        const result = await saleService.getAll();
+        expect(result).to.be.equal(payload);
+      });
+    });
+
+    describe('Get sale by id', () => {
+      const payload = [
+        { 
+          "date": "2021-09-09T04:54:29.000Z",
+          "product_id": 1,
+          "quantity": 2
+        },
+        {
+          "date": "2021-09-09T04:54:54.000Z",
+          "product_id": 2,
+          "quantity": 2
+        }
+      ];
+      before(() => {
+        sinon.stub(saleModel, 'getById').resolves(payload);
+      });
+
+      after(() => {
+        saleModel.getById.restore();
+      });
+
+      it('Returns correct sale', async () => {
+        const result = await saleService.getById(1);
+        expect(result).to.be.equal(payload);
+      });
+    });
+
+    describe('Updating sale', () => {
+      before(() => {
+        sinon.stub(saleModel, 'update').resolves(undefined);
+      });
+
+      after(() => {
+        saleModel.update.restore();
+      });
+
+      it('Returns correct sale', async () => {
+        const result = await saleService.update(1, 3, 2);
+        expect(result).to.be.undefined;
+      });
+    });
+
+    describe('Removing sale', () => {
+      describe('If there is no sale with that id', () => {
+        before(() => {
+          sinon.stub(saleModel, 'getById').resolves([]);
+        });
+  
+        after(() => {
+          saleModel.getById.restore();
+        });
+  
+        it('Returns correct sale', async () => {
+          const result = await saleService.remove(5);
+          expect(result).to.be.null;
+        });
+      });
+
+      describe('Sale removed', () => {
+        const payload = [
+          {
+            "date": "2021-09-09T04:54:29.000Z",
+            "product_id": 1,
+            "quantity": 2
+          },
+          {
+            "date": "2021-09-09T04:54:54.000Z",
+            "product_id": 2,
+            "quantity": 2
+          }
+        ];
+
+        before(() => {
+          sinon.stub(saleModel, 'getById').resolves(payload);
+          sinon.stub(saleModel, 'remove').resolves();
+          sinon.stub(productService, 'getById').resolves({ quantity: 2 });
+          sinon.stub(productService, 'updateQuant').resolves();
+        });
+  
+        after(() => {
+          saleModel.getById.restore();
+          saleModel.remove.restore();
+          productService.getById.restore();
+          productService.updateQuant.restore();
+        });
+  
+        it('Removed Successfully', async () => {
+          const result = await saleService.remove(1);
+          expect(result).to.be.equal(payload);
+        });
+      });
+    });
   });
 });
